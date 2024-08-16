383eb2d9e295ed014e5da85bff67a292f88eddff
Cassey Gempesaw <catgempesaw@addu.edu.ph>
2024-08-16 11:52:11 +0800

    updated based on additional routes/controllers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./routes/productRoutes.js');
+const roleRoutes = require('./routes/roleRoutes.js');
+const employeeRoutes = require('./routes/employeeRoutes.js');
+const customerRoutes = require('./routes/customerRoutes.js');
+const userAccountRoutes = require('./routes/userAccountRoutes.js');
+
 
 // Middleware to parse JSON request bodies
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/', productRoutes);
+app.use('/', roleRoutes);
+app.use('/', employeeRoutes);
+app.use('/', customerRoutes);
+app.use('/', userAccountRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
