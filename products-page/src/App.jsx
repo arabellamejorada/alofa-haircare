1eac03329367384f8fe1363d7972763fd74ae9b4
Arabella Mejorada <arabella.mejorada@gmail.com>
2024-09-16 10:21:42 +0800

    added cart summary, table, and shopping cart page

--- products-page/src/App.jsx
+++ products-page/src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from './shared/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import Products from './pages/Products.jsx';
 import FAQs from './pages/FAQs.jsx'
-
 import './App.css';
+import ShoppingCart from './pages/ShoppingCart.jsx';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/products" element={<Products />} />
         <Route path="/frequently-asked-questions" element={<FAQs/>} />
+        <Route path="/shoppingcart" element={<ShoppingCart/>} />
 
       </Routes>
     </Router>
--- products-page/src/shared/Navbar.jsx
+++ products-page/src/shared/Navbar.jsx
@@ -15,7 +15,8 @@ const Navbar = () => {
 
             {/* navigation pages */}
             <div className="text-lg font-semibold text-alofa-pink sm:flex items-center gap-8 hidden">
-              <Link to="/" className="hover:text-pink-700 items-baseline gap-2">Home</Link>
+              
+              <Link to="/" className="hover:text-pink-700 items-baseline gap-2 pl-3">Home</Link>
               {/* <p>|</p> */}
               <Link to="/products" className="hover:text-pink-700 flex items-baseline gap-2">Products</Link>
               {/* <p>|</p> */}
@@ -30,11 +31,11 @@ const Navbar = () => {
             <a href="/" className="hover:text-pink-700 flex items-center gap-2">Login</a>
             <p>|</p>
             <a href="/" className="hover:text-pink-700 flex items-center gap-2">Sign Up</a>
-            <a href="/" className="flex items-center gap-5">
+            <Link to="/shoppingcart" className="flex items-center gap-5">
                 <div className="text-[#FE699F] p-3 rounded-full transition-colors duration-300 hover:bg-[#FE699F] hover:text-white">
                 <FaShoppingCart/>
                 </div>
-            </a>
+            </Link>
           </div>
         </nav>
     </header>
