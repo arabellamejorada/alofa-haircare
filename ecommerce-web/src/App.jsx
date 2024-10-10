2e8a7713970d57f1b7f23aa6733f3dd5a7e9e82f
Arabella Mejorada <arabella.mejorada@gmail.com>
2024-10-10 14:14:52 +0800

    initialized checkout page

--- ecommerce-web/src/App.jsx
+++ ecommerce-web/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './components/CartContext.jsx';
-import Navbar from './shared/Navbar.jsx';
-import Home from './pages/Home.jsx';
-import Products from './pages/Products.jsx';
-import FAQs from './pages/FAQs.jsx';
-import ShoppingCart from './pages/ShoppingCart.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './components/CartContext.jsx'
+import Navbar from './shared/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Products from './pages/Products.jsx'
+import FAQs from './pages/FAQs.jsx'
+import ShoppingCart from './pages/ShoppingCart.jsx'
 import CartItem from './components/CartItem.jsx'
+// import Checkout from './pages/Checkout.jsx'
 import './App.css';
 
 function App() {
