b07541fc3d5175cc214c73111f8dd07e283502b1
Cassey Gempesaw <catgempesaw@addu.edu.ph>
2024-10-19 22:20:24 +0800

    add update delete in cartitems

--- ecommerce-web/src/api/cart.js
+++ ecommerce-web/src/api/cart.js
@@ -13,13 +13,37 @@ export const createOrGetCart = async (customer_id) => {
 };
 
 // ADD ITEM TO CART
-export const addItemToCart = async (variation_id, quantity) => {
+export const addCartItem = async (variation_id, quantity) => {
     try {
         const response = await axios.post('/cart/add-item', { variation_id, quantity });
         console.log('Item added to cart: ', response.data);
         return response.data;
     } catch (error) {
         console.error('Error adding item to cart: ', error);
+        throw error;
+    }
+};
+
+// UPDATE ITEM IN CART
+export const updateCartItem = async (cart_item_id, quantity) => {
+    try {
+        const response = await axios.put(`/cart/update-item/${cart_item_id}`, { quantity });
+        console.log('Item updated in cart: ', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating item in cart: ', error);
+        throw error;
+    }
+};
+
+// DELETE ITEM FROM CART
+export const deleteCartItem = async (cart_item_id) => {
+    try {
+        const response = await axios.delete(`/cart/delete-item/${cart_item_id}`);
+        console.log('Item deleted from cart: ', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting item from cart: ', error);
         throw error;
     }
 };
