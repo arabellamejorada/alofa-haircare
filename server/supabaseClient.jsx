45cb3c1cf3913720ef9a2c548fd45e72d04ad0ef
Arj <jraptabudlong@addu.edu.ph>
2024-10-31 17:10:48 +0800

    fixed server security leak

--- server/supabaseClient.jsx
+++ server/supabaseClient.jsx
@@ -1,9 +1,8 @@
 // for backend Admin privileges
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://hqzdxvlsgjkhtavvkmus.supabase.co"; // Add your Supabase URL to environment variables
-const supabaseServiceKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxemR4dmxzZ2praHRhdnZrbXVzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODk3NjY2MCwiZXhwIjoyMDQ0NTUyNjYwfQ.gI5brcM4vYefoUlpzfYVvuP3AOaxTcu_AFaW1lisbHA"; // Add your service role key to environment variables
+const supabaseUrl = process.env.REACT_APP_SUPABASE_REST_URL; // Add your Supabase URL to environment variables
+const supabaseServiceKey = process.env.REACT_APP_SUPERADMIN_SECRET_KEY; // Add your service role key to environment variables
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
