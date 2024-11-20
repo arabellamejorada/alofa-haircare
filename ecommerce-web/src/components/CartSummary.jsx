9cb54dfa5b8cd61045ef161b780d716740dbf610
Cassey Gempesaw <catgempesaw@addu.edu.ph>
2024-11-20 23:18:36 +0800

    update: update link navigation upon clicking checkout button for logged in/out customers

--- ecommerce-web/src/components/CartSummary.jsx
+++ ecommerce-web/src/components/CartSummary.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const CartSummary = ({ subtotal, user }) => {
+const CartSummary = ({ cartItems, subtotal, user }) => {
   return (
     <div className="bg-white rounded-lg shadow-lg p-6 mt-4">
       <div className="flex justify-between mb-4">
@@ -18,18 +18,27 @@ const CartSummary = ({ subtotal, user }) => {
           )}
         </span>
       </div>
-      {user ? (
-        <Link to="/checkout">
-          <button className="font-extrabold text-white w-full py-2 rounded-full focus:outline-none shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-gradient-to-b from-[#FE699F] to-[#F8587A] hover:bg-gradient-to-b hover:from-[#F8587A] hover:to-[#FE699F]">
-            CHECK OUT
-          </button>
-        </Link>
+      {cartItems.length > 0 ? (
+        user ? (
+          <Link to="/checkout">
+            <button className="font-extrabold text-white w-full py-2 rounded-full focus:outline-none shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-gradient-to-b from-[#FE699F] to-[#F8587A] hover:bg-gradient-to-b hover:from-[#F8587A] hover:to-[#FE699F]">
+              CHECK OUT
+            </button>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <button className="font-extrabold text-white w-full py-2 rounded-full focus:outline-none shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-gradient-to-b from-[#FE699F] to-[#F8587A] hover:bg-gradient-to-b hover:from-[#F8587A] hover:to-[#FE699F]">
+              CHECK OUT
+            </button>
+          </Link>
+        )
       ) : (
-        <Link to="/login">
-          <button className="font-extrabold text-white w-full py-2 rounded-full focus:outline-none shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-gradient-to-b from-[#FE699F] to-[#F8587A] hover:bg-gradient-to-b hover:from-[#F8587A] hover:to-[#FE699F]">
-            CHECK OUT
-          </button>
-        </Link>
+        <button
+          disabled
+          className="font-extrabold text-white w-full py-2 rounded-full focus:outline-none shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-gradient-to-b from-[#FE699F] to-[#F8587A] hover:bg-gradient-to-b hover:from-[#F8587A] hover:to-[#FE699F] opacity-50 cursor-not-allowed"
+        >
+          CHECK OUT
+        </button>
       )}
     </div>
   );
