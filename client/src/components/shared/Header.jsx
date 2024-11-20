a167dd17fdfb452051ddabea5a35b19a8a19816e
Arj <jraptabudlong@addu.edu.ph>
2024-11-20 18:39:20 +0800

    header profile adjustment

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from "../../supabaseClient";
 import { ClipLoader } from "react-spinners";
 import Modal from "../modal/Modal"; // Adjust the import path as necessary
+import { toast } from "sonner";
+
 
 export default function Header({ onMenuClick }) {
   const navigate = useNavigate();
@@ -143,10 +145,10 @@ export default function Header({ onMenuClick }) {
 
       setUserMetadata(profile);
       setIsEditing(false);
-      alert("Profile updated successfully");
+      toast.success("Profile updated successfully.");
     } catch (error) {
       console.error("Error updating profile:", error);
-      alert("Error updating profile: " + error.message);
+      toast.error("Error updating profile: " + error.message);
     }
   };
 
@@ -190,153 +192,156 @@ export default function Header({ onMenuClick }) {
             <ClipLoader size={50} color={"#E53E3E"} loading={true} />
           </div>
         ) : userMetadata ? (
-          <div className="p-4">
-            <div className="flex flex-col font-heading text-alofa-pink text-[3rem] mb-6">
-              <div className="mb-6">Profile</div>
-              <div className="flex justify-center">
-                <FaUserCircle className="text-[8rem] text-alofa-pink" />
-              </div>
+          <div className="p-6 max-w-lg mx-auto">
+            {/* Modal Header */}
+            <div className="flex flex-row items-center mb-6 gap-2">
+              <h2 className="text-3xl font-black text-alofa-pink">
+                Employee Profile
+              </h2>
             </div>
 
-            <div className="flex flex-col gap-6">
-              {/* Profile Fields */}
-              <div className="grid items-center gap-4 grid-cols-[0.5fr_1fr]">
-                <div className="font-semibold text-gray-600 w-[15rem]">
+            {/* Profile Fields */}
+            <div className="space-y-4">
+              {/* First Name */}
+              <div className="flex items-center">
+                <label className="w-32 font-bold text-gray-700">
                   First Name:
-                </div>
-                <div className="flex justify-end">
-                  {isEditing ? (
-                    <input
-                      type="text"
-                      value={formData.first_name}
-                      onChange={(e) =>
-                        setFormData({
-                          ...formData,
-                          first_name: e.target.value,
-                        })
-                      }
-                      placeholder="First Name"
-                      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                  ) : (
-                    <span className="text-gray-800">
-                      {userMetadata.first_name}
-                    </span>
-                  )}
-                </div>
-
-                <div className="font-semibold text-gray-600 w-[15rem]">
-                  Last Name:
-                </div>
-                <div className="flex justify-end">
-                  {isEditing ? (
-                    <input
-                      type="text"
-                      value={formData.last_name}
-                      onChange={(e) =>
-                        setFormData({
-                          ...formData,
-                          last_name: e.target.value,
-                        })
-                      }
-                      placeholder="Last Name"
-                      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                  ) : (
-                    <span className="text-gray-800">
-                      {userMetadata.last_name}
-                    </span>
-                  )}
-                </div>
-
-                <div className="font-semibold text-gray-600 w-[15rem]">
-                  Email:
-                </div>
-                <div className="flex justify-end">
-                  {isEditing ? (
-                    <input
-                      type="email"
-                      value={formData.email}
-                      onChange={(e) =>
-                        setFormData({
-                          ...formData,
-                          email: e.target.value,
-                        })
-                      }
-                      placeholder="Email"
-                      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                  ) : (
-                    <span className="text-gray-800">{userMetadata.email}</span>
-                  )}
-                </div>
-
-                <div className="font-semibold text-gray-600 w-[15rem]">
-                  Position:
-                </div>
-                <div className="flex justify-end">
-                  <span className="text-gray-800">
-                    {userMetadata.role_id === 2
-                      ? "Admin"
-                      : userMetadata.role_id === 3
-                        ? "Employee"
-                        : "User"}
-                  </span>
-                </div>
-
-                {isEditing && (
-                  <>
-                    <div className="font-semibold text-gray-600 w-[15rem]">
-                      Password:
-                    </div>
-                    <div className="flex justify-end">
-                      <input
-                        type="password"
-                        value={formData.password}
-                        onChange={(e) =>
-                          setFormData({
-                            ...formData,
-                            password: e.target.value,
-                          })
-                        }
-                        placeholder="New Password"
-                        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
-                      />
-                    </div>
-                  </>
-                )}
-              </div>
-
-              {/* Edit and Save/Cancel Buttons */}
-              <div className="flex justify-end gap-4">
-                {!isEditing ? (
-                  <button
-                    onClick={() => setIsEditing(true)}
-                    className="px-4 py-2 text-gray-500 hover:text-alofa-pink "
-                  >
-                    Edit
-                  </button>
+                </label>
+                {isEditing ? (
+                  <input
+                    type="text"
+                    value={formData.first_name}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        first_name: e.target.value,
+                      })
+                    }
+                    placeholder="First Name"
+                    className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-alofa-pink"
+                  />
                 ) : (
-                  <>
-                    <button
-                      onClick={handleSave}
-                      className="px-4 py-2 bg-alofa-pink text-white font-semibold rounded-lg hover:bg-alofa-dark transition"
-                    >
-                      Save
-                    </button>
-                    <button
-                      onClick={handleCancel}
-                      className="px-4 py-2 bg-gray-300 text-gray-800 rounded-lg hover:bg-gray-400 transition duration-200"
-                    >
-                      Cancel
-                    </button>
-                  </>
+                  <span className="flex-1 text-gray-500">
+                    {userMetadata.first_name}
+                  </span>
                 )}
               </div>
+
+              {/* Last Name */}
+              <div className="flex items-center">
+                <label className="w-32 font-bold text-gray-700">
+                  Last Name:
+                </label>
+                {isEditing ? (
+                  <input
+                    type="text"
+                    value={formData.last_name}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        last_name: e.target.value,
+                      })
+                    }
+                    placeholder="Last Name"
+                    className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-alofa-pink"
+                  />
+                ) : (
+                  <span className="flex-1 text-gray-500">
+                    {userMetadata.last_name}
+                  </span>
+                )}
+              </div>
+
+              {/* Email */}
+              <div className="flex items-center">
+                <label className="w-32 font-bold text-gray-700">Email:</label>
+                {isEditing ? (
+                  <input
+                    type="email"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        email: e.target.value,
+                      })
+                    }
+                    placeholder="Email"
+                    className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-alofa-pink"
+                  />
+                ) : (
+                  <span className="flex-1 text-gray-500">
+                    {userMetadata.email}
+                  </span>
+                )}
+              </div>
+
+              {/* Position */}
+              <div className="flex items-center">
+                <label className="w-32 font-bold text-gray-700">
+                  Position:
+                </label>
+                <span className="flex-1 text-gray-500">
+                  {userMetadata.role_id === 2
+                    ? "Admin"
+                    : userMetadata.role_id === 3
+                      ? "Employee"
+                      : "User"}
+                </span>
+              </div>
+
+              {/* Password (Only when Editing) */}
+              {isEditing && (
+                <div className="flex items-center">
+                  <label className="w-32 font-bold text-gray-700">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    value={formData.password}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        password: e.target.value,
+                      })
+                    }
+                    placeholder="New Password"
+                    className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-alofa-pink"
+                  />
+                </div>
+              )}
+            </div>
+
+            {/* Action Buttons */}
+            <div className="mt-6 flex justify-end space-x-4">
+              {!isEditing ? (
+                <button
+                  onClick={() => setIsEditing(true)}
+                  className="px-4 py-2 bg-alofa-pink text-white font-semibold rounded-lg hover:bg-alofa-dark transitio"
+                >
+                  Edit
+                </button>
+              ) : (
+                <>
+                  <button
+                    onClick={handleSave}
+                    className="px-4 py-2 bg-alofa-pink text-white font-semibold rounded-lg hover:bg-alofa-dark transitio"
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={handleCancel}
+                    className="px-4 py-2 bg-gray-300 text-gray-700 font-semibold rounded-lg hover:bg-gray-400 transition"
+                  >
+                    Cancel
+                  </button>
+                </>
+              )}
             </div>
           </div>
         ) : (
-          <p>Loading user information...</p>
+          <div className="flex items-center justify-center p-20">
+            <ClipLoader size={50} color={"#E53E3E"} loading={true} />
+          </div>
         )}
       </Modal>
     </div>
