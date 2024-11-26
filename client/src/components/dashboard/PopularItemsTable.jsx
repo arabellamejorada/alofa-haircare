2c9523738b2269093b094dfe08309ed536280b9f
Arj <jraptabudlong@addu.edu.ph>
2024-11-26 13:21:11 +0800

    images

--- client/src/components/dashboard/PopularItemsTable.jsx
+++ client/src/components/dashboard/PopularItemsTable.jsx
@@ -76,7 +76,7 @@ const PopularItemsTable = ({ fetchOrdersWithItems, selectedMonth }) => {
                 Item
               </th>
               <th className="px-6 py-2 border-b-2 border-gray-200 bg-alofa-pink text-white text-right text-sm font-semibold">
-                Total Orders
+                Orders
               </th>
             </tr>
           </thead>
